2db4aec09b628fcf57be3ff999a5cbb3bf0540b2
MitsukiMoon <jodiehoh@gmail.com>
2016-09-08 08:12:55 -0700

    Added Lab 3

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,15 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int x = 5;
+		int y = 5;
+		int z = 5;
+		int M = x*y;
+		int C = (2*x*y) + (2*x*z) + (2*y*z);
+		
+		System.out.println("The multiplication of " + x + " and " + y + " is " + M);
+		System.out.println("The area of a rectangular prisim is " + C);
+		
+	}
+}
