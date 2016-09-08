9935c71b0b66238788c1cc4c6e259c0c4b6c0c22
MitsukiMoon <jodiehoh@gmail.com>
2016-09-08 08:02:42 -0700

    Added Files

---
+++ APLesson_01/ASCII_1.java
@@ -0,0 +1,27 @@
+public class ASCII_1
+{
+	public static void main(String[]args)
+	{ 
+		System.out.println("|)  _.._  (| /_");
+		System.out.println("(o ^ __ ^ o) Z_7");
+		System.out.println("(*________*)Z7");
+		System.out.println("*          *");
+		
+		System.out.println("RRRRRRRRRRRR");
+		System.out.println("  RRR        RRR");
+		System.out.println("  RRR         RRR");
+		System.out.println("  RRR          RRR");
+		System.out.println("  RRR         RRR ");
+		System.out.println("  RRR         RRR   ");
+		System.out.println("  RRRRRRRRRRRRR");
+		System.out.println("  RRR      RRR");
+		System.out.println("  RRR       RRR");
+		System.out.println("  RRR        RRR");
+		System.out.println("  RRR         RRR");
+		System.out.println("  RRR		   RRR ");
+		System.out.println("  RRR           RRR");
+		System.out.println("RRRRRRR          RRRRRRRRR");
+		
+		
+	}
+}
