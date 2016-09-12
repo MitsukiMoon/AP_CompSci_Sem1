9035345a59bc40f3f38b756c57e0dcf113fd80b0
MitsukiMoon <jodiehoh@gmail.com>
2016-09-12 08:20:55 -0700

    Added Quiz

---
+++ Quiz_01/Answers.java
@@ -0,0 +1,29 @@
+public class Answers
+{
+	public static void main(String[]args)
+	{
+		int num = 45;
+		
+		String pres = "Abraham Lincoln";
+		String quote = "\"Whatever you are, be a good one.\"";
+		
+		int h = 45;
+		int w = 64;
+		
+		
+		System.out.println ("Question 1: " + "IronMan has " + num + " kinds of weapons in his suit.");
+		System.out.println ("Question 2: " + pres + " once said, " + quote);
+		System.out.println ("Question 3: " + "The area of your rectangle is ..." + h*w );
+		System.out.println ("Question 4: " + "It is because the code is looking for the next int, not the next string.");
+		System.out.println ("Question 5: " + "C");
+		System.out.println ("Question 6: " + "A");
+		System.out.println ("Question 7: " + "There is an extra quotation mark that needs to be erased.");
+		System.out.println ("Question 8: " + "\n" + "This one goes on top" + "\n" + "This one goes on bottom.");
+		System.out.println ("Question 9: " + "\n" +  "a" + "\t" + "b" + "\t" + "c" + "\n" + "d" + "\t" + "e" + "\t" + "f" + "\n" + "g" + "\t" + "h" + "\t" + "i");
+		System.out.println ("Question 10:" + "The answer returned is a decimal, not an integer. ")
+		
+		
+		
+		
+	}
+}
