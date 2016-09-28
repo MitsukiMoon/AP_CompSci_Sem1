fa2eeb5e20e7204340cfb1e9f330c0e5a0918d40
MitsukiMoon <jodiehoh@gmail.com>
2016-09-28 08:50:21 -0700

    Updated Address

--- APLesson_02/Lab_02.java
+++ APLesson_02/Lab_02.java
@@ -7,8 +7,16 @@ public class Lab_02
 		int z = 5;
 		int M = x*y;
 		int C = (2*x*y) + (2*x*z) + (2*y*z);
+		String name = "Jodie Hoh";
+		String address = "123 Main Street";
+		String street = "Beverly Hills, CA 90210";
+		
 		
 		System.out.println("The multiplication of " + x + " and " + y + " is " + M);
+		System.out.println(name + "\n" + address + "\n" + street);
+		
+		
+		
 		System.out.println("The area of a rectangular prisim is " + C);
 		
 	}
